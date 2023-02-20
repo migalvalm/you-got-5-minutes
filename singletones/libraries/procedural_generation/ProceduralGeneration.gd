76f4888d5b926a58fe5a7fe3aa3a747b89bca86f
migalvalm <59445459+migalvalm@users.noreply.github.com>
2023-02-20 03:54:50 +0000

    Make singletone functions static

--- singletones/libraries/procedural_generation/ProceduralGeneration.gd
+++ singletones/libraries/procedural_generation/ProceduralGeneration.gd
@@ -2,19 +2,19 @@ extends Node
 
 var decoration_list: Array
 
-## Room Generation Pure Function
+## Room Generator Function
 func generate_room(tile_floor, tile_wall, tile_decoration, width: int, height: int) -> void:
 	generate_floor_tile(tile_floor, width, height)
 	generate_wall_tile(tile_wall, width, height)
 	generate_decoration_tile(tile_decoration, height, width)
 
-## Generator Functions
-func generate_floor_tile(tilemap, width: int, height: int) -> void:
+## Generators Functions
+static func generate_floor_tile(tilemap, width: int, height: int) -> void:
 	for x in width:
 		for y in height:
 			place_cell(x, y, tilemap.TILES_ID, tilemap)
 
-func generate_wall_tile(tilemap, width: int, height: int) -> void:
+static func generate_wall_tile(tilemap, width: int, height: int) -> void:
 	randomize()
 
 	place_walls(width, height, tilemap)
@@ -25,16 +25,16 @@ func generate_decoration_tile(tilemap, width:int, height:int) -> void:
 	
 	for decoration in tilemap.AMOUNT:
 		var random_pos: Vector2 = random_spawn_position(height, width)
-		place_cell(random_pos.x, -random_pos.y, tilemap.TILES_ID, tilemap)
+		place_cell(int(random_pos.x), -int(random_pos.y), tilemap.TILES_ID, tilemap)
 
 ## Procedural Wall Generation Helpers
-func place_corners(width, height, tilemap):
+static func place_corners(width, height, tilemap):
 	place_cell(0, 0, tilemap.CORNER_LEFT_TOP, tilemap)
 	place_cell(width, 0, tilemap.CORNER_RIGHT_TOP, tilemap)
 	place_cell(0, height, tilemap.CORNER_LEFT_BOTTOM, tilemap)
 	place_cell(width, height, tilemap.CORNER_RIGHT_BOTTOM, tilemap)
 
-func place_walls(width, height, tilemap):
+static func place_walls(width, height, tilemap):
 	for x in width:
 		#Fill Top 
 		place_cell(x, 0, tilemap.MIDDLE_TOP_BOT, tilemap)
@@ -56,5 +56,5 @@ func random_spawn_position(width: int, height: int) -> Vector2:
 	
 	return random_pos
 
-func place_cell(x: int, y: int, tiles: Array, tilemap):
+static func place_cell(x: int, y: int, tiles: Array, tilemap) -> void:
 	tilemap.set_cell(x, y, tiles[randi() % tiles.size()])
